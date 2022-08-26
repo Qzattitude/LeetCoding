ef93f128fa25d35c8f8ebed066d9376f04d73bba
Farzin Mukit <meemmeen1990@gmail.com>
2022-08-26 15:07:55 +0600

    Add project files.

---
+++ Probelem Solving/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Probelem_Solving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Damn this is not good!");
+        }
+    }
+}
